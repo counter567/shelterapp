d921ea42b6bbb7cae86285d774749092c92b480f
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-03-15 18:10:11 +0100

    add shelterapp wprdpress plugin

--- wp-plugin/shelterapp/shelterapp.php
+++ wp-plugin/shelterapp/shelterapp.php
@@ -1,2 +1,16 @@
 <?php
+require_once(__DIR__ . '/vendor/autoload.php');
 
+
+$apiInstance = new OpenAPI\Client\Api\AnimalEventResourceApi(
+    // If you want use custom http client, pass your client which implements `GuzzleHttp\ClientInterface`.
+    // This is optional, `GuzzleHttp\Client` will be used as default.
+    new GuzzleHttp\Client()
+);
+
+try {
+    $result = $apiInstance->animalEventsCountGet();
+    print_r($result);
+} catch (Exception $e) {
+    echo 'Exception when calling AnimalEventResourceApi->animalEventsCountGet: ', $e->getMessage(), PHP_EOL;
+}
