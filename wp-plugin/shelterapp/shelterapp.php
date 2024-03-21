ec7503dc84d4f2b89ac0c4f84cdb0a3e40004e28
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-03-21 16:50:43 +0100

    added activation / deactivation hooks

--- wp-plugin/shelterapp/shelterapp.php
+++ wp-plugin/shelterapp/shelterapp.php
@@ -14,3 +14,20 @@ Text Domain: shelterapp
 
 include_once(__DIR__ . '/includes/index.php');
 include_once(__DIR__ . '/blocks/index.php');
+
+
+
+/* Main Plugin File */
+function sa_activate()
+{
+    global $SHELTERAPP_GLOBAL_ANIMAL;
+    $SHELTERAPP_GLOBAL_ANIMAL->activate_plugin();
+}
+register_activation_hook(__FILE__, 'sa_activate');
+
+function sa_deactivate()
+{
+    global $SHELTERAPP_GLOBAL_ANIMAL;
+    $SHELTERAPP_GLOBAL_ANIMAL->deactivate_plugin();
+}
+register_deactivation_hook(__FILE__, 'sa_deactivate');
