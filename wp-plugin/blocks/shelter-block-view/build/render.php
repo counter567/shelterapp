548710674b6a47bb91965cc13a4f379696ec2701
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-03-21 17:15:59 +0100

    add wpApiSettings to use the rest API

--- wp-plugin/blocks/shelter-block-view/build/render.php
+++ wp-plugin/blocks/shelter-block-view/build/render.php
@@ -4,6 +4,12 @@
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
 print_r($attributes);
+
+wp_localize_script('create-block-shelter-block-view-view-script', 'wpApiSettings', array(
+	'root' => esc_url_raw(rest_url()),
+	'nonce' => wp_create_nonce('wp_rest')
+));
+
 ?>
 <div data-type="<?php echo isset($attributes['type']) ? $attributes['type'] : '' ?>" <?php echo get_block_wrapper_attributes(); ?>>
 	<?php esc_html_e('Shelter Block View – hello from a dynamic block!', 'shelter-block-view'); ?>
