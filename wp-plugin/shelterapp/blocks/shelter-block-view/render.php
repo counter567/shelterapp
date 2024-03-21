bf9b6d2dbf86ec932e7f64cad75200980d1b0fd8
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-03-21 16:49:01 +0100

    build block files

--- wp-plugin/shelterapp/blocks/shelter-block-view/render.php
+++ wp-plugin/shelterapp/blocks/shelter-block-view/render.php
@@ -3,7 +3,8 @@
 /**
  * @see https://github.com/WordPress/gutenberg/blob/trunk/docs/reference-guides/block-api/block-metadata.md#render
  */
+print_r($attributes);
 ?>
-<p <?php echo get_block_wrapper_attributes(); ?>>
+<div data-type="<?php echo isset($attributes['type']) ? $attributes['type'] : '' ?>" <?php echo get_block_wrapper_attributes(); ?>>
 	<?php esc_html_e('Shelter Block View – hello from a dynamic block!', 'shelter-block-view'); ?>
-</p>
+</div>
