3a37dbdb930eee04168611ddf9a90e524a810324
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-06-05 14:13:40 +0200

    fix buttons changing size

--- frontend/src/components/DropDown.tsx
+++ frontend/src/components/DropDown.tsx
@@ -22,10 +22,10 @@ const DropDown = ({ items, value, defaultValue, callback }: DropDownProps) => {
   };
   return (
     <>
-      <div>
+      <div style={{minWidth: '20vw'}}>
         <button
           data-dropdown-toggle="dropdown"
-          className="text-white w-full whitespace-nowrap justify-between max-w-80 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white w-full whitespace-nowrap justify-between bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           type="button"
           onClick={() => setIsOpen((value) => !value)}
         >
