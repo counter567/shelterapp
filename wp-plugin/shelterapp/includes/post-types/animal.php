239d62c1b6e7a817df7229667e04c195e49f6b53
Jan Sobotta <cv.jsobotta+dev@googlemail.com>
2024-07-14 19:16:35 +0200

    stabelized sync

--- wp-plugin/shelterapp/includes/post-types/animal.php
+++ wp-plugin/shelterapp/includes/post-types/animal.php
@@ -279,9 +279,6 @@ class ShelterappAnimals
 
         outLog('***********************************************');
         outLog('saving a post! Update to backend!');
-        outLog($post_id);
-        outLog($post);
-        outLog($update);
     }
 
     function init_rest()
@@ -921,6 +918,19 @@ add_action('before_delete_post', function($post_id){
         $attachments = get_attached_media('image', $post_id);
         foreach ($attachments as $attachment) {
             wp_delete_attachment($attachment->ID, true);
+        }
+    }
+    $shelterapp_id = get_post_meta($post_id, 'shelterapp_id', true);
+    if(isset($shelterapp_id) && !empty($shelterapp_id)) {
+        outLog('Delete animal with id ' . $shelterapp_id);
+        try{
+            sa_sync_delete_animal($shelterapp_id);
+        } catch(Exception $e) {
+            outLog('There was an error during delete:');
+            outLog($e->getMessage());
+            outLog(array_map(function($entry){
+                return $entry['function'];
+            }, $e->getTrace()));
         }
     }
 });
